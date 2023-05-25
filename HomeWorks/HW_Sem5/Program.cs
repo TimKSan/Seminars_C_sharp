e1b85cc90676fa50c72ee949169e320fb620964c
Timur <kokanbaev1@gmail.com>
2023-05-25 17:59:32 +0300

    started task 3

--- HomeWorks/HW_Sem5/Program.cs
+++ HomeWorks/HW_Sem5/Program.cs
@@ -50,3 +50,78 @@ int[] ShowSumOddNum(int size = 6, int minVal = -10, int maxVal = 11)
 
 ShowSumOddNum();
 */
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+// double[] DiffMaxMin (int size = 5, int minVal = 0, int maxVal = 100)
+// {
+//     double[] newArray = new double[size];
+    
+//     for (int i = 0; i < size; i++)
+//     {
+//         newArray[i] = new Random().Next(minVal, maxVal) + new Random().NextDouble();
+//         double minNum = newArray[0];
+//         //Console.WriteLine($"arr min - {minNum}");
+//         double maxNum = newArray[0];
+//         double diffMinMax = maxNum - minNum;
+
+//         if (newArray[i] < minNum) 
+//         {
+//             minNum = newArray[i];
+//             Console.WriteLine($"min - {minNum}");
+//         }
+//         else if (newArray[i] > maxNum) 
+//         {
+//             maxNum = newArray[i];
+//             Console.WriteLine($"max - {maxNum}");
+//         }
+//         //Console.WriteLine(diffMinMax);
+//     }
+    
+    
+//     return newArray;
+// }
+
+int[] DiffMaxMin (int size = 5, int minVal = 0, int maxVal = 100)
+{
+    int[] newArray = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(minVal, maxVal);
+        int minNum = newArray[0];
+        int maxNum = newArray[0];
+    
+        if (newArray[i] < minNum) 
+        {
+            minNum = newArray[i];
+            //Console.WriteLine($"min - {minNum}");
+        }
+        else if (newArray[i] > maxNum) 
+        {
+            maxNum = newArray[i];
+            //Console.WriteLine($"max - {maxNum}");
+        }
+        int diffMinMax = maxNum - minNum;
+    }
+    
+    Console.WriteLine(diffMinMax);
+    
+    return newArray;
+}
+
+void PrinArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] myArray = DiffMaxMin();
+PrinArray(myArray);
+
+//Console.WriteLine(new Random().Next(0, 100) + new Random().NextDouble());
