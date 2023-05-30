3d9573080ea518c6f78123e9f35138e6aba423ee
Timur <kokanbaev1@gmail.com>
2023-05-30 18:58:13 +0300

    end all task for the seminar 6

--- HomeWorks/HW_Sem6/Program.cs
+++ HomeWorks/HW_Sem6/Program.cs
@@ -46,8 +46,35 @@ PrintArray(myArray);
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-void ShowsIntersectionPoint(int b1, int k1, int b2, int k2)
+void ShowsIntersectionPoint(double k1, double b1, double k2, double b2)
 {
-    int x = (b2 - b1) / (k1 - k2);
-    int y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+
+    if (k1 == k2)
+    {
+        Console.Write("Заданные прямые не пересекаются!");
+    }
+
+    else if ((k1 == k2) && (b1 == b2))
+    {
+        Console.Write("Прямые параллельны");
+    }
+
+    else Console.Write($"Точка пересечения прямых: ({x}; {y})");
+
 }
+
+Console.WriteLine("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+ShowsIntersectionPoint(k1, b1, k2, b2);
